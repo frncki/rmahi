9efb2fb0f672dcccdc27ae7d899237af27ac7ba3
Franek <franciszek.mirecki@gmail.com>
2021-09-13 19:01:32 +0200

    removal of unnecessary code and addition of proxy in package.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,7 @@
-import CryptoJS from "crypto-js";
-
 function App() {
 
-  const encodeImgName = (name) => {
-    let ciphertext = CryptoJS.AES.encrypt(name, 'jebacpis').toString(); // szyfrowanie nazwy przy pomocy algorytmu AES z biblioteki crypto-js
-    ciphertext = encodeURIComponent(ciphertext); // zamiana niedozwolonych znakow na hexy
-    return ciphertext;
-  }
-
-  const decodeImgName = (ciphertext) => {
-    ciphertext = decodeURIComponent(ciphertext) // zamiana wcześniej zmienionych hexów na "niedozwolone znaki"
-    let bytes  = CryptoJS.AES.decrypt(ciphertext, 'jebacpis'); // rozszyfrowanie nazwy przy pomocy algorytmu AES z biblioteki crypto-js
-    let originalText = bytes.toString(CryptoJS.enc.Utf8);
-    return originalText;
-  }
-  
-  let ciphertext = encodeImgName('zdjecie_profesorka.bmp');
-  let originalText = decodeImgName(ciphertext);
-  
-  console.log(ciphertext);
-  console.log(originalText);
-
   return (
-    <p>asd*</p>
+    <p>dziaua</p>
   );
 }
 
