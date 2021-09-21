0091b38e0e93fe8e2e3f30ea865e64d4236b1dbe
Franek <franciszek.mirecki@gmail.com>
2021-09-21 12:19:48 +0200

    encryption key in .env

--- api/src/hashing.js
+++ api/src/hashing.js
@@ -4,13 +4,13 @@ const encodeImgName = (name) => {
     let ciphertext = CryptoJS.AES.encrypt(name, process.env.ENCRYPTION_KEY).toString(); // szyfrowanie nazwy przy pomocy algorytmu AES z biblioteki crypto-js
     ciphertext = encodeURIComponent(ciphertext); // zamiana niedozwolonych znakow na hexy
     return ciphertext;
-  }
+}
 
-  const decodeImgName = (ciphertext) => {
+const decodeImgName = (ciphertext) => {
     ciphertext = decodeURIComponent(ciphertext); // zamiana wcześniej zmienionych hexów na "niedozwolone znaki"
-    let bytes  = CryptoJS.AES.decrypt(ciphertext, process.env.ENCRYPTION_KEY); // rozszyfrowanie nazwy przy pomocy algorytmu AES z biblioteki crypto-js
+    let bytes = CryptoJS.AES.decrypt(ciphertext, process.env.ENCRYPTION_KEY); // rozszyfrowanie nazwy przy pomocy algorytmu AES z biblioteki crypto-js
     let originalText = bytes.toString(CryptoJS.enc.Utf8);
     return originalText;
-  }
+}
 
-  export { encodeImgName, decodeImgName }
+export {encodeImgName, decodeImgName}
