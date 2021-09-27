75ffaa466e67ad69479e6004946ab752fdadf760
Franek <franciszek.mirecki@gmail.com>
2021-09-27 16:21:17 +0200

    moved base64 methods to seperate file

---
+++ api/public/js/base64.js
@@ -0,0 +1,40 @@
+import path from 'path';
+import fs from 'fs';
+// Methods for  POST
+const extractBase64Data = (data) => {
+    const regexForBase64 = /^data:image\/([\w+]+);base64,([\s\S]+)/;
+    const match = data.match(regexForBase64);
+  
+    if (!match) {
+      throw new Error('image base64 data error');
+    }
+  
+    return match[2];
+  }
+  
+  const writeBase64Image = async (data, destpath, name) => {
+    const base64Image = extractBase64Data(data);
+    const filepath = path.join(destpath, name);
+    await fs.promises.writeFile(filepath, base64Image, { encoding: 'base64' });
+  
+    return filepath;
+  };
+  
+  const writeImages = (imagesData, newIdSavingPath) => {
+    imagesData.forEach((imageData) => {
+      const { base64Image, fileName } = imageData;
+      writeBase64Image(base64Image, newIdSavingPath, fileName)
+    });
+  }
+  // Methods for GET
+  const createBase64Image = (file) => {
+    const reader = new FileReader();
+    return new Promise(function (resolve, reject) {
+        reader.onload = function (event) {
+            resolve(event.target.result)
+        }
+        reader.readAsDataURL(file);
+    })
+  }
+
+export { writeImages };
--- api/routes/images.js
+++ api/routes/images.js
@@ -2,37 +2,12 @@ import express from "express";
 import path from 'path';
 import fs from 'fs';
 import { processImages } from "../public/js/exifRemoving";
+import { writeImages } from "../public/js/base64";
 
 const router = express.Router();
 
-const savingDirPath = path.join(__dirname, `../${process.env.INPUT_IMAGES}`)
-const outDirPath = path.join(__dirname, `../${process.env.OUTPUT_IMAGES}`)
-
-const extractBase64Data = (data) => {
-  const reg = /^data:image\/([\w+]+);base64,([\s\S]+)/;
-  const match = data.match(reg);
-
-  if (!match) {
-    throw new Error('image base64 data error');
-  }
-
-  return match[2];
-}
-
-const writeBase64Image = async (data, destpath, name) => {
-  const base64Image = extractBase64Data(data);
-  const filepath = path.join(destpath, name);
-  await fs.promises.writeFile(filepath, base64Image, { encoding: 'base64' });
-
-  return filepath;
-};
-
-const writeImages = (imagesData, newIdSavingPath) => {
-  imagesData.forEach((imageData) => {
-    const { base64Image, fileName } = imageData;
-    writeBase64Image(base64Image, newIdSavingPath, fileName)
-  });
-}
+const savingDirPath = path.join(__dirname, `../${process.env.INPUT_IMAGES}`);
+const outDirPath = path.join(__dirname, `../${process.env.OUTPUT_IMAGES}`);
 
 // Route Controller for uploading base64 image file
 router.post('/base64', (req, res) => {
